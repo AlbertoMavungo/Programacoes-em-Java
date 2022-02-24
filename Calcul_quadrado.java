fa6212f4d88cab3c52f1bcc9a7e4fd87c9159e1b
alberto <mavungo1998@gmail.com>
2022-02-24 18:03:49 -0300

    primeiro commit

---
+++ Calcul_quadrado.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Calcul_quadrado {
+        public static void main(String[] args) {
+        //declarando 
+        int n1, n2, n3, n4, resultado;
+        //entrada
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primero número: "));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número: "));
+        n3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número: "));
+        n4 = Integer.parseInt(JOptionPane.showInputDialog("Digite o quarto número: "));
+        //operação
+        resultado = (n1*n1) + (n2*n2) + (n3*n3) + (n4*n4);
+        //imprimindo
+        String c = String.format("(%f*%f) + (%f*%f) + (%f*%f) + (%f*%f) = %f", n1,n2,n3,n4,resultado);
+            JOptionPane.showMessageDialog(null,c);
+    }    
+    
+}
---
+++ Cotacao_dolar.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class Cotacao_dolar {
+    public static void main(String[] args) {
+        //declacao das variáveis 
+        Double Quantidade, Cotacao, Reais;
+        //entrada de dados
+        Quantidade = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade dos dólares: "));
+        Cotacao = Double.parseDouble(JOptionPane.showInputDialog("Digite a cotação atual: "));
+        //operação
+        Reais = Quantidade * Cotacao;
+        //Iimprimindo
+        String c = String.format("%.2f + %.2f = %.2f", Quantidade,Cotacao,Reais);
+        JOptionPane.showMessageDialog(null,c);
+    }    
+}
